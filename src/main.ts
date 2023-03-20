77e9c0cf8c53a1d6a4354ee5ee1c021086cc1cac
kevinbatard <kevin.batard4064@gmail.com>
2023-03-20 10:50:45 +0100

    feat mise en place doc swagger dans le main et global pipes ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix('api/');
+  const config = new DocumentBuilder()
+    .setTitle('Japan travels')
+    .setDescription('Partagez vos souvenirs de voyage, ou préparez le votre !')
+    .setVersion('1.0')
+    .addTag('Japan')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(8000);
 }
 bootstrap();
